1cafb73a4962667240ee473265110d8427818666
Selim Dursun <m.seldur@gmail.com>
2021-02-08 22:09:38 +0100

    Frontend: unwanted old functional logics are deleted

--- frontend/src/epics/index.js
+++
@@ -1,9 +0,0 @@
-import { combineEpics } from 'redux-observable';
-import { counterEpic } from './counter' 
-import { loginEpics } from './login' 
-
-export const rootEpic = combineEpics(
-    counterEpic,
-    ...loginEpics
-  );
-  
