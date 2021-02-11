710a79c831776012ba3779cfb74c33e62e831bb9
Selim Dursun <m.seldur@gmail.com>
2021-02-11 21:32:08 +0100

    Frontend: basic router logic is implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import VoteComponent from './containers/vote';
-import HeaderComponent from './containers/header';
+import AuthRoute from './components/autoroute';
+import HomeComponent from './containers/home/home';
+import HeaderComponent from './containers/header/header';
+import MeComponent from './containers/me/me';
+
+import './App.css';
 
 const store = configureStore();
 
@@ -13,10 +17,24 @@ class App extends Component {
   render() {
     return (
     <Provider store={store}>
+        <Router>
+
       <div className="App">
-        <HeaderComponent/>
-        <VoteComponent />
+      <HeaderComponent/>
+        <div className="container">
+          <Switch>
+            <AuthRoute path="/login" type="guest">
+              <div>Login Page</div>
+            </AuthRoute>
+            <AuthRoute path="/me" type="private">
+              <MeComponent></MeComponent>
+            </AuthRoute>
+            <Route path="/" component={HomeComponent} />
+          </Switch>
+        </div>
+        <div>Footer</div>
       </div>
+      </Router>
     </Provider>
     );
   }
