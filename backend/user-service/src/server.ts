89bbbd1940698a2e2c95c0e2d0baec6848ce2f2f
Selim Dursun <m.seldur@gmail.com>
2021-02-14 18:01:37 +0100

    Backend: User-service refactoring, delete unused env variables

--- backend/user-service/src/server.ts
+++ backend/user-service/src/server.ts
@@ -1,20 +1,16 @@
 import cors from 'cors';
 import express from 'express';
-import {sequelize} from './sequelize';
-
-import {IndexRouter} from './controllers/v0/index.router';
-
+import { sequelize } from './sequelize';
+import { IndexRouter } from './controllers/v0/index.router';
 import bodyParser from 'body-parser';
-import {config} from './config/config';
-import {V0_USER_MODELS} from './controllers/v0/model.index';
-
+import { V0_USER_MODELS } from './controllers/v0/model.index';
 
 (async () => {
   await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
 
   const app = express();
-  const port = process.env.USER_SERVICE_PORT || 8080;
+  const port = 8080;
 
   app.use(bodyParser.json());
 
@@ -38,7 +34,7 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
 
   // Start the Server
   app.listen( port, () => {
-    console.log( `server running ${config.url}` );
+    console.log( `server running on ${port}` );
     console.log( `press CTRL+C to stop server` );
   } );
 })();
